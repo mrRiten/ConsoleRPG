ce575a529a6558d1a731092c2a4a755dd05f1b06
Igroman4ik <ni4000009@gmail.com>
2023-12-22 21:48:20 +0300

    Create new static void PlayerInfo in IUConsole Print PlayerInfo (color) Add text to about (color) Create command /quit Logo to center

--- ConsoleRPG/Program.cs
+++ ConsoleRPG/Program.cs
@@ -1,19 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace ConsoleRPG
 {
     internal class Program
     {
-        
+
         static void Main(string[] args)
         {
-
+            Console.Title = "ConsoleRPG";
             var ui = new UIConsole();
-            //list base entiti второй лист хранит base block все объекты классов не имеют внутренние названия для програмиста
             ui.PrintLogo();
             while (true)
             {
+                
                 ui.PrintMenu();
                 switch (ui.CurrentCom)
                 {
@@ -22,21 +21,25 @@ namespace ConsoleRPG
                         break;
 
                     case "/about":
+                        UIConsole.AboutProg();
                         break;
 
                     case "/quit":
+                        Environment.Exit(0);
                         break;
                 }
             }
         }
         static void StartGame()
         {
+            // Мб сделать отдельную функцию settings чтобы удобно управлять настройками player (и не только) и в будующем их маштабировать?
             int[] pos = { 0, 0 };
-            var player = new Player(pos, "&");
+            var player = new Player(pos, "&", "player", 100, 10);
             var map = new Map(player);
             while (true)
             {
                 map.PrintMap();
+                UIConsole.PlayerInfo(player);
                 player.Move(Console.ReadKey());
                 map.MoveEntety(player);
 
